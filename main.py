fe365472ed0c32a8efd3a722c843437e28a7e0f1
Roman Zalewski <r.a.zalewski@gmail.com>
2025-10-22 20:48:13 +0000

    refining classes

--- main.py
+++ main.py
@@ -2,12 +2,15 @@
 # The goal is that the KDTree cna be called like an sklearn object
 # Like kd = KDTree(), then appended onto with the append command
 # The network of classes should provide a decent framework with safety
-import networkx
-import numpy as np
-from sklearn.neighbors import KNeighborsClassifier
-import pandas as pd
+
+#imports I think we'll need:
+#import networkx
+#import numpy as np
+#from sklearn.neighbors import KNeighborsClassifier
+#import pandas as pd
 from dataclasses import dataclass
 import typing as t
+
 
 data = ('name','age','profession','salary')
 
@@ -37,15 +40,57 @@ class Node:
                 exit
         self.value = Data(**kwargs)
 
+    def __str__(self):
+        return f"Node containing {self.value}"
+
+    def __gt__(self, other, by):
+        for f in self.value.Fields():
+            if f.name == by:
+                type_of_by = f.type
+        if type_of_by == int:
+            if self.value.by > self.value.by:
+                return True
+            if self.value.by < self.value.by:
+                return False
+        elif type_of_by == str: #implemented backwards so A is greater then B
+            if self.value.by > self.value.by:
+                return False
+            if self.value.by < self.value.by:
+                return True
+        else:
+            KDError("Error in function")
+            exit
+            
+    def __lt__(self == other, by):
+        for f in self.value.Fields():
+            if f.name == by:
+                type_of_by = f.type
+        if type_of_by == int:
+            if self.value.by < self.value.by:
+                return True
+            if self.value.by > self.value.by:
+                return False
+        elif type_of_by == str: #implemented backwards so A is greater then B
+            if self.value.by < self.value.by:
+                return False
+            if self.value.by > self.value.by:
+                return True
+        else:
+            KDError("Error in function")
+            exit
+            
+
     def depth(self):
-        ldepth = depth(first_node.left)
-        rdepth = depth(first_node.right)
+        ldepth = depth(self.left)
+        rdepth = depth(self.right)
         return (1 + max(ldepth + rdepth))
 
 
 class KDTree:
-    def __init__(self, head):
+
+    def __init__(self, head, data):
         self.head = head
+        self.data = data
 
     def append(self, data:Node): 
         max_depth = self.head.depth()
@@ -61,3 +106,5 @@ class KDTree:
             curr_node.right = data
         if data.attr < curr_node.attr:
             curr_node.left = data
+    def __str__(self):
+        return f"KD tree with dimension {len(self.data)} and depth {self.head.depth}"
