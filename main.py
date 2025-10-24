46919c63f5ce3a9e9c996e0bc1d74fcedc58ac61
Roman Zalewski <r.a.zalewski@gmail.com>
2025-10-24 15:45:18 +0000

    progress

--- main.py
+++ main.py
@@ -1,14 +1,9 @@
-#TODO: im scared to test it
-# The goal is that the KDTree cna be called like an sklearn object
-# Like kd = KDTree(), then appended onto with the append command
-# The network of classes should provide a decent framework with safety
-
 #imports I think we'll need:
 #import graphviz
 #import numpy as np
 #from sklearn.neighbors import KNeighborsClassifier
 #import pandas as pd
-from dataclasses import dataclass
+from dataclasses import dataclass, fields
 import typing as t
 
 
@@ -33,6 +28,7 @@ class Node:
         self.value = None
         self.right = None
         self.left = None
+        self.depth = None
         for key in list(kwargs.keys()):
             if key not in data:
                 KDerror(f"{key} not recognized in keyword arguments")
@@ -43,8 +39,9 @@ class Node:
     def __str__(self):
         return f"Node containing {self.value}"
 
+
     def __gt__(self, other, by):
-        for f in self.value.Fields():
+        for f in self.value.fields():
             if f.name == by:
                 type_of_by = f.type
         if type_of_by == int:
@@ -62,7 +59,7 @@ class Node:
             exit
             
     def __lt__(self, other, by):
-        for f in self.value.Fields():
+        for f in self.value.fields():
             if f.name == by:
                 type_of_by = f.type
         if type_of_by == int:
@@ -79,7 +76,6 @@ class Node:
             KDError("Error in function")
             exit
             
-
     def depth(self):
         ldepth = 0
         rdepth = 0
@@ -91,28 +87,33 @@ class Node:
 
 
 class KDTree:
-
+# there might be an indexing problem, unsure honestly. Root node should be 0, but I think in the for loop its represented as 1. TODO
     def __init__(self, head):
         self.head = head
+        self.head.depth = 0
 
-    def append(self, data:Node): 
+    def append(self, new_node:Node):
+        print(self.head)
         max_depth = self.head.depth()
         curr_node = self.head
-        curr_depth = 0
-        for i in range(curr_depth):
-            attr = data[i+1%len(data)]
+        curr_depth = 1
+        for i in range(max_depth):
+            attr = [i.name for i in fields(new_node.value)][i%len(data)]
             print(f'attr={attr}')
-            if getattr(data, attr) > getattr(curr_node, attr):
+            if getattr(new_node.value, attr) > getattr(curr_node.value, attr) and curr_node.right!=None:
                 curr_node = curr_node.right
-            if getattr(data, attr) < getattr(curr_node, attr):
+            if getattr(new_node.value, attr) < getattr(curr_node.value, attr) and curr_node.left!=None:
                 curr_node = curr_node.left
-        if getattr(data, attr) > getattr(curr_node, attr):
-            curr_node.right = data
-        if getattr(data, attr) < getattr(curr_node, attr):
-            curr_node.left = data
-    def __str__(self):
-        return f"KD tree might work!"
+        if getattr(new_node.value, attr) > getattr(curr_node.value, attr):
+            curr_node.right = new_node
+            curr_node.right.depth = i+1#idk if these are accurate yet
+        if getattr(new_node.value, attr) < getattr(curr_node.value, attr):
+            curr_node.left = new_node
+            curr_node.left.depth = i+1#idk if these are accurate yet
 
+    def __str__(self):
+        for i in range(self.head.depth()):
+            pass #TODO: implement, thinking like an ascii image with just the name
 
 
 
