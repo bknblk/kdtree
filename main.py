1db5991194a4c9713198932b0a28e08727b18091
Roman Zalewski <r.a.zalewski@gmail.com>
2025-10-27 16:44:01 +0000

    testing avl features

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-#imports I think we'll need:
 import graphviz
 from dataclasses import dataclass, fields, asdict
 import typing as t
+from collections import deque
 
 
 data = ('name','age','profession','salary')
@@ -33,6 +33,7 @@ class Node:
 
     def __str__(self):
         return f"Node containing {self.value}"
+
             
     def find_depth(self):
         ldepth = 0
@@ -58,58 +59,107 @@ class Node:
         attr = [i.name for i in node_fields][iteration%len(node_fields)]
         if getattr(new_node.value, attr) >= (getattr(self.value, attr)):
             if self.right:
-                self.right.append(new_node, iteration=iteration+1)
+                self.right = self.right.append(new_node, iteration=iteration+1)
             else:
                 self.right = new_node
-        if getattr(new_node.value, attr) < (getattr(self.value, attr)):
+        elif getattr(new_node.value, attr) < (getattr(self.value, attr)):
             if self.left:
-                self.left.append(new_node, iteration=iteration+1)
+                self.left = self.left.append(new_node, iteration=iteration+1)
             else:
                 self.left = new_node
+        return self._balance()
 
     def vis(self, ax):
         out = ''
         for key,value in asdict(self.value).items():
             out += f'{key}: {value}\n'
         ax.node(self.value.name,out)
-        if self.right:
-            ax.edge(self.value.name, self.right.value.name)
-            self.right.vis(ax)
         if self.left:
             ax.edge(self.value.name, self.left.value.name)
             self.left.vis(ax)
+        if self.right:
+            ax.edge(self.value.name, self.right.value.name)
+            self.right.vis(ax)
+    
+    def _get_balance(self):
+        lheight = 0
+        rheight = 0
+        if self.left:
+            lheight = self.left.find_depth()
+        if self.right:
+            rheight = self.right.find_depth()
+        return lheight - rheight
+
+    def _rotate_l(self):
+        new_head = self.right
+        self.right = new_head.left
+        new_head.left = self
+        return new_head
+
+
+    def _rotate_r(self):
+        new_head = self.left
+        self.left = new_head.right
+        new_head.right = self
+        return new_head
+    
+    def _rotate_rl(self):
+        self.right = self.right._rotate_r()
+        return self._rotate_l()
+
+    def _rotate_lr(self):
+        self.left = self.left._rotate_l()
+        return self._rotate_r()
+
+    def _balance(self):
+        bal = self._get_balance()
+        if bal > 1:
+            cbal = self.left._get_balance()
+            if cbal >= 0:
+                return self._rotate_r()
+            if cbal < 0:
+                return self._rotate_lr()
+        if bal < -1:
+            cbal = self.right._get_balance()
+            if cbal <= 0:
+                return self._rotate_l()
+            if cbal > 0:
+                return self._rotate_rl()
+        return self
         
+
 
             
 
 if __name__ == '__main__' :
     head = Node(name = 'Roman', age = 20, profession = 'Army', salary = 2)
-    head.append(Node(name = 'Jonah', age = 21, profession = 'Data Scientist', salary = 0))
-    head.append(Node(name = 'Ty', age = 22, profession = 'USMC', salary = 3600))
-    head.append(Node(name = 'Nathan', age=19, profession = 'nerd', salary = 0))
-    head.append(Node(name = 'Evan', age=21, profession = 'Pilot', salary = 100))
-    head.append(Node(name = 'Faisal',age=30, profession = 'Professor', salary = 20))
-    head.append(Node(name = 'Robert', age= 27, profession = 'Contruction', salary = 400))
-    head.append(Node(name='Sophia', age=24, profession='Designer', salary=55))
-    head.append(Node(name='Liam', age=35, profession='Manager', salary=95))
-    head.append(Node(name='Emma', age=22, profession='Intern', salary=15))
-    head.append(Node(name='Oliver', age=29, profession='Developer', salary=80))
-    head.append(Node(name='Ava', age=31, profession='Analyst', salary=65))
-    head.append(Node(name='Noah', age=26, profession='Consultant', salary=70))
-    head.append(Node(name='Isabella', age=23, profession='Assistant', salary=35))
-    head.append(Node(name='Ethan', age=33, profession='Architect', salary=90))
-    head.append(Node(name='Mia', age=25, profession='Researcher', salary=50))
-    head.append(Node(name='Lucas', age=27, profession='Technician', salary=45))
-    head.append(Node(name='Charlotte', age=30, profession='Director', salary=110))
-    head.append(Node(name='Mason', age=24, profession='Coordinator', salary=40))
-    head.append(Node(name='Amelia', age=32, profession='Specialist', salary=68))
-    head.append(Node(name='James', age=29, profession='Administrator', salary=52))
-    head.append(Node(name='Harper', age=26, profession='Scientist', salary=72))
-    head.append(Node(name='Benjamin', age=34, profession='Supervisor', salary=85))
-    head.append(Node(name='Evelyn', age=23, profession='Trainee', salary=25))
-    head.append(Node(name='Logan', age=28, profession='Operator', salary=48))
-    head.append(Node(name='Abigail', age=31, profession='Executive', salary=105))
+    head = head.append(Node(name = 'Jonah', age = 21, profession = 'Data Scientist', salary = 0))
+    head = head.append(Node(name = 'Ty', age = 22, profession = 'USMC', salary = 3600))
+    head = head.append(Node(name = 'Nathan', age=19, profession = 'nerd', salary = 0))
+    head = head.append(Node(name = 'Evan', age=21, profession = 'Pilot', salary = 100))
+    head = head.append(Node(name = 'Faisal',age=30, profession = 'Professor', salary = 20))
+    head = head.append(Node(name = 'Robert', age= 27, profession = 'Contruction', salary = 400))
+    head = head.append(Node(name='Sophia', age=24, profession='Designer', salary=55))
+    head = head.append(Node(name='Liam', age=35, profession='Manager', salary=95))
+    head = head.append(Node(name='Emma', age=22, profession='Intern', salary=15))
+    head = head.append(Node(name='Oliver', age=29, profession='Developer', salary=80))
+    head = head.append(Node(name='Ava', age=31, profession='Analyst', salary=65))
+    head = head.append(Node(name='Noah', age=26, profession='Consultant', salary=70))
+    head = head.append(Node(name='Isabella', age=23, profession='Assistant', salary=35))
+    head = head.append(Node(name='Ethan', age=33, profession='Architect', salary=90))
+    head = head.append(Node(name='Mia', age=25, profession='Researcher', salary=50))
+    head = head.append(Node(name='Lucas', age=27, profession='Technician', salary=45))
+    head = head.append(Node(name='Charlotte', age=30, profession='Director', salary=110))
+    head = head.append(Node(name='Mason', age=24, profession='Coordinator', salary=40))
+    head = head.append(Node(name='Amelia', age=32, profession='Specialist', salary=68))
+    head = head.append(Node(name='James', age=29, profession='Administrator', salary=52))
+    head = head.append(Node(name='Harper', age=26, profession='Scientist', salary=72))
+    head = head.append(Node(name='Benjamin', age=34, profession='Supervisor', salary=85))
+    head = head.append(Node(name='Evelyn', age=23, profession='Trainee', salary=25))
+    head = head.append(Node(name='Logan', age=28, profession='Operator', salary=48))
+    head = head.append(Node(name='Abigail', age=31, profession='Executive', salary=105))
     head.show_tree()
     dot = graphviz.Digraph()
     head.vis(dot)
     dot.render('kdtree', format='png')
+    head.show_tree()
